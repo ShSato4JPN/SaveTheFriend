17db14cc4b19bd4f92c3f56f2099722b88142f97
ShSato4 <ShSato4.private.1221@gmail.com>
2020-09-12 20:45:18 +0900

    一時保存
    一時保存

--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,16 @@
 /*** EasyEncryptionのロジック ***/
 window.onload = function() {
     // 結果表示エリア
-    let txa_result = document.getElementById("txa-resutl");
-    // ファイルのパス
-    let txt_filepath = document.getElementById("in-filepath");
-    // 改行コード
-    let selLineFeed = document.getElementById("cmb-linefeed");
-   // 処理開始ボタン
+    let txa_result = document.getElementById("txa-result");
+    // 選択された改行コード
+    let selLinefeed = document.getElementById("cmb-linefeed");
+    // 処理開始ボタン
     let btn_proc = document.getElementById("btn-proc");
-    // CSV出力ボタン
+    // ファイル名表示用
+    let in_fileName = document.getElementById("in-fileName");
+    // ファイル読み込みボタン
+    let in_file = document.getElementById("in-file");
+    // csv出力ボタン
     let btn_csv = document.getElementById("btn-csv");
     // アップロードファイル
     let uploadFile;
@@ -16,13 +18,13 @@ window.onload = function() {
     // Event : ファイル読み込み
     let reader = new FileReader();
     reader.addEventListener("load", function() {
-        txt_result.value = reader.result;
+        txa_result.value = reader.result;
     });
 
     // Event : ファイル選択ボタン
-    btn_dialog.addEventListener("change",  function(event) {
-        uploadFile = event.target.files;
-        txt_filepath.value = uploadFile[0].name;
-        reader.readAsText( uploadFile[0]);
-    });
+      in_file.addEventListener("change",  function(event) {
+          uploadFile = event.target.files;
+          in_fileName.value = uploadFile[0].name;
+          reader.readAsText( uploadFile[0]);
+      });
 }
