e97dbb756671d2919aa5f79b5de31a0633cce217
ShSato4JPN <48849803+ShSato4JPN@users.noreply.github.com>
2020-09-13 17:29:35 +0900

    ファイル読み込み処理実装

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,7 @@
 /*** EasyEncryptionのロジック ***/
 window.onload = function() {
     // 結果表示エリア
-    let txa_result = document.getElementById("txa-result");
+    let txa_resutl = document.getElementById("txa-resutl");
     // 選択された改行コード
     let selLinefeed = document.getElementById("cmb-linefeed");
     // 処理開始ボタン
@@ -12,19 +12,30 @@ window.onload = function() {
     let in_file = document.getElementById("in-file");
     // csv出力ボタン
     let btn_csv = document.getElementById("btn-csv");
-    // アップロードファイル
-    let uploadFile;
 
-    // Event : ファイル読み込み
-    let reader = new FileReader();
-    reader.addEventListener("load", function() {
-        txa_result.value = reader.result;
+    // Event : ファイル選択処理
+     in_file.addEventListener("change",  function(e) {
+        // FileReader クラスに対応しているか
+        if(!(window.FileReader)) return;
+        // ファイルリストを取得
+        let file_list = e.target.files;
+        // ファイル読み込み処理
+        var file_reader = new FileReader();
+        // ファイル読み込み成功時
+        file_reader.addEventListener("load", function(e) {
+              txa_resutl.textContent  = file_reader.result;
+              console.log(file_reader.result);
+        });
+        // ファイル読み込み失敗時
+        file_reader.addEventListener("error",function(e) {
+            alert("読み込みが失敗した");
+            console.log(file_reader.error);
+        });
+        // ファイル読み込み開始
+        file_reader.readAsText(file_list[0]);
     });
 
-    // Event : ファイル選択ボタン
-      in_file.addEventListener("change",  function(event) {
-          uploadFile = event.target.files;
-          in_fileName.value = uploadFile[0].name;
-          reader.readAsText( uploadFile[0]);
-      });
+    btn_csv.addEventListener("click", function(e) {
+        alert("hello");
+    });
 }
